85a1f201d55b15aadec4df5dc95f2832e0e861ce
steal <steal@IST361>
2018-03-29 13:06:35 -0500

    Worked on implementing a timer. Also changing stuff over to constants

---
+++ CYBR4450-KeyLogger/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef _CONSTANTS_H
+#define _CONSTANTS_H
+extern const int BUFFER_SIZE = 5000;
+extern const int LAST_WINDOW_SIZE = 256;
+extern const int TWO_MINUTES_IN_SECONDS = 60 * 2;
+#endif // !_CONSTANTS_H
